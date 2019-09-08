9c0da61872707e90f5be98566124976f0d6a27e2
LappenLike <lappenlikedevelopment@gmail.com>
2019-09-08 19:42:45 +0200

    Initial commit

---
+++ app/src/main/java/com/smithy/lappenlike/workingtitle/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.smithy.lappenlike.workingtitle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+    }
+}
---
+++ app/src/main/java/com/smithy/lappenlike/workingtitle/Profile.java
@@ -0,0 +1,13 @@
+package com.smithy.lappenlike.workingtitle;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_activity);
+    }
+}
