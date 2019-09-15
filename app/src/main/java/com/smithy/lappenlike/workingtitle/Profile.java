1383194087339a17a6c8fd3636760f5da73376ce
LappenLike <lappenlikedevelopment@gmail.com>
2019-09-15 20:09:21 +0200

    Pic Upload

--- app/src/main/java/com/smithy/lappenlike/workingtitle/Profile.java
+++ app/src/main/java/com/smithy/lappenlike/workingtitle/Profile.java
@@ -1,9 +1,15 @@
 package com.smithy.lappenlike.workingtitle;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.Nullable;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -13,7 +19,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
+
 public class Profile extends BaseActivity {
+
+    private final int CHOOSE_IMAGE = 100;
 
     private FirebaseAuth mAuth;
     private FirebaseUser user;
@@ -22,6 +32,9 @@ public class Profile extends BaseActivity {
     private DatabaseReference databaseRef;
 
     private TextView tv_profileName;
+    private ImageView iv_profileImage;
+
+    private Uri profileImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +48,10 @@ public class Profile extends BaseActivity {
         databaseRef = fireInstance.getReference("users/"+userId);
 
         tv_profileName = findViewById(R.id.tv_profileName);
+        iv_profileImage = findViewById(R.id.iv_profileImage);
 
         initProfileData();
+        initProfileImage();
     }
 
     private void initProfileData(){
@@ -54,4 +69,31 @@ public class Profile extends BaseActivity {
         });
     }
 
+    private void initProfileImage(){
+        iv_profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Profilbild Auswahl"), CHOOSE_IMAGE);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == CHOOSE_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null ){
+            profileImageUri = data.getData();
+            try {
+                Bitmap imageBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), profileImageUri);
+                iv_profileImage.setImageBitmap(imageBitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
